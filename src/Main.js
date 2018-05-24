ad43136f1998ae52a024c78ac52f8aa4b713f09a
Sam Coe <coe6@purdue.edu>
2018-05-24 09:36:26 -0400

    Add deploy script

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends React.Component {
 
         this.state = {
             currentNote: this.blankNote(),
-            notes: storedNotes,
+            notes: storedNotes || [],
         }
     }
 
